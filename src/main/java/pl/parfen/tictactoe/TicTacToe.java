67a5c84753489059a67b7017c4ddee03f2ead2d5
Dmitry Parfenovich <89684073451@mail.ru>
2022-11-09 19:42:54 +0100

    create game table

--- src/main/java/pl/parfen/tictactoe/TicTacToe.java
+++ src/main/java/pl/parfen/tictactoe/TicTacToe.java
@@ -1,2 +1,83 @@
-package pl.parfen.tictactoe;public class TicTacToe {
+package pl.parfen.tictactoe;
+
+import java.util.Random;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Use the following mapping table to specify a cell using number from 1 to 9:");
+
+        char [][] gameTable = {
+                {' ',' ',' '},
+                {' ',' ',' '},
+                {' ',' ',' '},
+        };
+        printTableMapping();
+        if (new Random().nextBoolean()) {
+            makeComputerMove(gameTable);
+            printGameTable(gameTable);
+        }
+
+        while (true) {
+            makeUserMove(gameTable);
+            printGameTable(gameTable);
+            if (isUserWin(gameTable)) {
+                System.out.println("YOU WIN!");
+                break;
+            }
+            if (isDraw(gameTable)) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+            makeComputerMove(gameTable);
+            printGameTable(gameTable);
+            if (isComputerWin(gameTable)) {
+                System.out.println("COMPUTER WIN!");
+                break;
+            }
+            if (isDraw(gameTable)) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+            break;
+        }
+        System.out.println("GAME OVER");
+    }
+
+    private static void printTableMapping() {
+        char [][] mappingTable = {
+                {'7','8','9'},
+                {'4','5','6'},
+                {'1','2','3'},
+        };
+        printGameTable(mappingTable);
+    }
+    private static void printGameTable(char [][] gameTable) {
+        for (int i= 0; i <  3; i++) {
+            System.out.println("-------------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("|  "+gameTable[i][j]+"  ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------------");
+    }
+    private static void makeUserMove(char [][] gameTable) {
+
+    }
+
+    private static void makeComputerMove(char [][] gameTable) {
+
+    }
+
+    private static boolean isUserWin(char [][] gameTable) {
+        return false;
+    }
+
+    private static boolean isComputerWin(char [][] gameTable) {
+        return false;
+    }
+
+    private static boolean isDraw(char [][] gameTable) {
+        return false;
+    }
 }
